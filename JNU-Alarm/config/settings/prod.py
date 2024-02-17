8c8323009d23f306161305d39913bad5e65ad653
Rayleigh Choi <86937253+Rayleigh190@users.noreply.github.com>
2024-02-17 23:10:09 +0900

    Revert "set: RDS 연결 설정"

--- JNU-Alarm/config/settings/prod.py
+++ JNU-Alarm/config/settings/prod.py
@@ -2,21 +2,16 @@ from .base import *
 
 Debug = False
 
-ALLOWED_HOSTS = [env('AWS_IP')]
+ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # engine: mysql
-        'NAME' : env('DB_NAME'),
-        'USER' : env('DB_USER'),
-        'PASSWORD' : env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '3306',
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': env,
+#         'USER': env('USER'),
+#         'PASSWORD': env('PASSWORD')
+#     }
+# }
 
 ## CRONTAB
 CRONTAB_DJANGO_SETTINGS_MODULE = 'config.settings.prod'

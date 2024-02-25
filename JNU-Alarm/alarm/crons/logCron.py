d6e1ce7992b8646f1c19d11c424d2c06a8190e45
rayleigh <rayleigh190@gmail.com>
2024-02-25 22:07:10 +0900

    feat: uwsgi 로그 파일명 날짜로 변경하기 구현

--- JNU-Alarm/alarm/crons/logCron.py
+++ JNU-Alarm/alarm/crons/logCron.py
@@ -1,10 +1,17 @@
 import os
 from django.conf import settings
-from datetime import datetime 
+from datetime import datetime, timedelta
 # BASE_DIR에 접근하여 파일 경로 구성
 
 def change_cron_log_name():
   os.rename(
     os.path.join(settings.BASE_DIR, 'alarm/log/cron_log/today.log'), 
     os.path.join(settings.BASE_DIR, f'alarm/log/cron_log/{datetime.now().strftime("%Y-%m-%d")}.log')
+  )
+
+def change_uwsgi_log_name():
+  yesterday = datetime.now() - timedelta(days=1)
+  os.rename(
+    os.path.join(settings.BASE_DIR, '/var/log/uwsgi/JNU-Alarm/today.log'), 
+    os.path.join(settings.BASE_DIR, f'/var/log/uwsgi/JNU-Alarm/{yesterday.strftime("%Y-%m-%d")}.log')
   )

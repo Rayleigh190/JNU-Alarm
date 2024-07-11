13496e54d16cf1b89ebd34378a05a069cc4ed4f6
rayleigh <rayleigh190@gmail.com>
2024-07-11 15:46:39 +0900

    feat: Shortcut 모델에 색코드 및 이용가능여부 추가

--- JNU-Alarm/dashboard/admin.py
+++ JNU-Alarm/dashboard/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Shortcut
 
 class ShortcutAdmin(admin.ModelAdmin):
-  list_display = ('name', 'is_webview', 'is_modal',)
+  list_display = ('name', 'is_available', 'is_webview', 'is_modal',)
   list_display_links = ('name',)
 
 admin.site.register(Shortcut, ShortcutAdmin)
--- JNU-Alarm/dashboard/models.py
+++ JNU-Alarm/dashboard/models.py
@@ -4,7 +4,9 @@ class Shortcut(models.Model):
   name = models.CharField(max_length=10)
   ios_image_name = models.CharField(max_length=20)
   aos_image_name = models.CharField(max_length=20)
+  color_code = models.CharField(max_length=6)
   link = models.URLField()
+  is_available = models.BooleanField()
   is_webview = models.BooleanField()
   is_modal = models.BooleanField()
 

24d9503329f0a9cf373ad6da789e82aa54ef3424
rayleigh <rayleigh190@gmail.com>
2024-07-12 12:05:08 +0900

    feat: 대시보드 API에 cache control 적용

--- JNU-Alarm/dashboard/views.py
+++ JNU-Alarm/dashboard/views.py
@@ -1,14 +1,17 @@
 from rest_framework import status
 from rest_framework.response import Response  
 from rest_framework.views import APIView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
 from .models import Shortcut, BannerAd
 from .serializer import ShortcutSerializer, BannerAdSerializer
 from django.utils import timezone
 import random
 
 class ShortcutView(APIView):
+  @method_decorator(cache_control(max_age=3600))
   def get(self, request):
-    shortcut = Shortcut.objects.all()
+    shortcut = Shortcut.objects.filter(is_available=True)
     serializer = ShortcutSerializer(shortcut, many=True)
     result_dic = {
       'success': True, 
@@ -18,6 +21,7 @@ class ShortcutView(APIView):
     return Response(result_dic, status=status.HTTP_200_OK)
   
 class BannerAdView(APIView):
+  @method_decorator(cache_control(max_age=10))
   def get(self, request):
     today = timezone.now()
     bannerAds = BannerAd.objects.filter(is_available=True, expiry_date__gte=today)

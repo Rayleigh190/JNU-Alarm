65b02786ba2073305992714f894c896eac99ede5
rayleigh <rayleigh190@gmail.com>
2024-07-14 19:08:52 +0900

    feat: 대시보드 이용가능여부 수정  admin action 구현

--- JNU-Alarm/dashboard/admin.py
+++ JNU-Alarm/dashboard/admin.py
@@ -3,20 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import Shortcut, BannerAd, Restaurant
 
+def make_available_action(modeladmin, request, queryset):
+    queryset.update(is_available=True)
+make_available_action.short_description = "선택된 아이템의 is_available을 True로 변경합니다."
+
+def make_unavailable_action(modeladmin, request, queryset):
+    queryset.update(is_available=False)
+make_unavailable_action.short_description = "선택된 아이템의 is_available을 False로 변경합니다."
+
+@admin.register(Shortcut)
 class ShortcutAdmin(admin.ModelAdmin):
   list_display = ('name', 'is_available', 'is_webview', 'is_modal',)
-  list_display_links = ('name',)
+  actions = [make_available_action, make_unavailable_action]
 
-admin.site.register(Shortcut, ShortcutAdmin)
-
+@admin.register(BannerAd)
 class BannerAdmin(admin.ModelAdmin):
   list_display = ('name', 'is_available', 'expiry_date')
-  list_display_links = ('name',)
+  actions = [make_available_action, make_unavailable_action]
 
-admin.site.register(BannerAd, BannerAdmin)
-
+@admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
   list_display = ('name', 'road_address', 'type', 'is_available',)
   list_display_links = ('name', 'road_address',)
-
-admin.site.register(Restaurant, RestaurantAdmin)
+  actions = [make_available_action, make_unavailable_action]

7665a13c17832a72bc04ab96625eefd7269260d6
rayleigh <rayleigh190@gmail.com>
2024-10-15 23:00:24 +0900

    change: prod DATABASES SQLite3로 변경

--- JNU-Alarm/config/settings/prod.py
+++ JNU-Alarm/config/settings/prod.py
@@ -6,15 +6,8 @@ ALLOWED_HOSTS = [env('AWS_IP')]
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # engine: mysql
-        'NAME' : env('DB_NAME'),
-        'USER' : env('DB_USER'),
-        'PASSWORD' : env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '3306',
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
